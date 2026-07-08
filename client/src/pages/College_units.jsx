f2e2d6e782468e0c1ba9cc7f45db70856377cc83
Mehadi Hassan Siddique <mehadi.hassan2744@gmail.com>
2026-07-08 22:24:25 +0530

    volunteer finish draft

--- client/src/pages/College_units.jsx
+++ client/src/pages/College_units.jsx
@@ -4,6 +4,41 @@ import { useNavigate } from "react-router-dom";
 
 function CollegeUnits() {
   const navigate = useNavigate();
+
+  const chief = [
+    {
+      id: 1,
+      name: "Nilotpal Mazumdar",
+      role: "Wing C Director",
+      image: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?auto=format&fit=crop&w=400&q=80",
+      age: "21",
+      quote: "With 20 years in international development, Elena crafts our overarching operational framework to ensure scalability and critical growth.",
+      serving_since: "21 july 2022",
+      bio: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Sed euismod, nunc ut aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.",
+      achievements:[
+        {
+            title: "Achievement 1",
+            year: "2022",
+        },
+        {
+            title: "Achievement 2",
+            year: "2023",
+        }
+      ],
+      qualifications: [
+        {
+            qual: "Qualification 1",
+            year: "2020",
+        },
+        {
+            qual: "Qualification 2",
+            year: "2021",
+        }
+      ] ,
+    },
+    
+  ];
+
   const directors = [
     {
       id: 1,
@@ -11,6 +46,7 @@ function CollegeUnits() {
       role: "Tech Cell",
       image: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?auto=format&fit=crop&w=400&q=80",
       bio: "With 20 years in international development, Elena crafts our overarching operational framework to ensure scalability and critical growth.",
+      back: 'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url("https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80")',
       path: "/team/tech_cell",
     },
     {
@@ -19,6 +55,7 @@ function CollegeUnits() {
       role: "Program & Research wing",
       image: "https://images.unsplash.com/photo-1560250097-0b93528c311a?auto=format&fit=crop&w=400&q=80",
       bio: "A pioneer in blockchain-for-good, Marcus leads our technical division, integrating transparent aid tracking into every project.",
+      back: 'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url("https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80")',
       path: "/team/program_research_wing",
     },
     {
@@ -27,6 +64,7 @@ function CollegeUnits() {
       role: "Media & PR Cell",
       image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80",
       bio: "Sarah bridges the gap between private capital and humanitarian need, building alliances that multiply our reach across five continents.",
+      back: 'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url("https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80")',
       path: "/team/media_pr_cell",
     },
     {
@@ -35,6 +73,7 @@ function CollegeUnits() {
       role: "Finance & Accounts Cell",
       image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80",
       bio: "Sarah bridges the gap between private capital and humanitarian need, building alliances that multiply our reach across five continents.",
+      back: 'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url("https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80")',
       path: "/team/finance_accounts_cell",
     },
     {
@@ -43,6 +82,7 @@ function CollegeUnits() {
       role: "Finance & Accounts Cell",
       image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80",
       bio: "Sarah bridges the gap between private capital and humanitarian need, building alliances that multiply our reach across five continents.",
+      back: 'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url("https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=400&q=80")',
       path: "/team/finance_accounts_cell",
     },
   ];
@@ -62,6 +102,47 @@ function CollegeUnits() {
           </p>
         </div>
 
+        <div className={styles.chiefGrid}>
+          {chief.map((member) => (
+            <div key={member.id} className={styles.chiefCard} onClick={()=> navigate(member.path || "/")}>
+              <div>
+              <div className={styles.chiefAvatarRingWrapper} onClick={()=> navigate(member.path || "/")}>
+                  <img src={member.image} alt={member.name} />
+              </div>
+              <p className={styles.chiefRole}>{member.role}</p>
+              <p className={styles.chiefQuote}>{member.quote}</p>
+              <div className={styles.chiefKeep}>
+                  <h3 className={styles.chiefName}>{member.name}</h3>
+                  <p className={styles.chiefAge}>Age: {member.age}</p>
+                  <p className={styles.chiefServingSince}>Serving Since: {member.serving_since}</p>
+                  <div className={styles.chiefQualifications}>
+                          <h4>Qualifications:</h4>
+                          <div className={styles.qualificationList}>
+                              {member.qualifications.map((qualification, index) => (
+                                  <li key={index}>{qualification.qual} ({qualification.year})</li>
+                              ))}
+                          </div>
+                  </div>
+                  <div className={styles.chiefAchievements}>
+                          <h4>Achievements:</h4>
+                          <div className={styles.achievementList}>
+                              {member.achievements.map((achievement, index) => (
+                                  <li key={index}>{achievement.title} ({achievement.year})</li>
+                              ))}
+                          </div>
+                  </div>
+                  <p className={styles.chiefBio1}>Bio:</p>
+                  <p className={styles.chiefBio}>{member.bio}</p>
+              </div>
+                  
+              </div>
+              <div className={styles.chiefSocials}>
+                <span>🌐</span>
+                <span>✉️</span>
+              </div>
+            </div>
+          ))}
+        </div>
 
         {/* Strategic Visionaries */}
         <div className={styles.teamSec}>
@@ -78,7 +159,7 @@ function CollegeUnits() {
 
           <div className={styles.visionariesGrid}>
             {directors.map((member) => (
-              <div key={member.id} className={styles.visionaryCard} onClick={()=> navigate(member.path || "/")}>
+              <div key={member.id} className={styles.visionaryCard} style={{ backgroundImage: member.back }} onClick={()=> navigate(member.path || "/")}>
                 <div>
                   <div className={styles.avatarRingWrapper} onClick={()=> navigate(member.path || "/")}>
                     <img src={member.image} alt={member.name} />
--- client/src/pages/Core_admin.jsx
+++ client/src/pages/Core_admin.jsx
@@ -4,6 +4,41 @@ import { useNavigate } from "react-router-dom";
 
 function CoreAdmin() {
   const navigate = useNavigate();
+
+  const chief = [
+    {
+      id: 1,
+      name: "Hrituporn ",
+      role: "Wing A Director",
+      image: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?auto=format&fit=crop&w=400&q=80",
+      age: "21",
+      quote: "With 20 years in international development, Elena crafts our overarching operational framework to ensure scalability and critical growth.",
+      serving_since: "21 july 2022",
+      bio: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc. Sed euismod, nunc ut aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nunc nisl eget nunc.",
+      achievements:[
+        {
+            title: "Achievement 1",
+            year: "2022",
+        },
+        {
+            title: "Achievement 2",
+            year: "2023",
+        }
+      ],
+      qualifications: [
+        {
+            qual: "Qualification 1",
+            year: "2020",
+        },
+        {
+            qual: "Qualification 2",
+            year: "2021",
+        }
+      ] ,
+    },
+    
+  ];
+
   const directors = [
     {
       id: 1,
@@ -38,6 +73,47 @@ function CoreAdmin() {
           </p>
         </div>
 
+        <div className={styles.chiefGrid}>
+          {chief.map((member) => (
+            <div key={member.id} className={styles.chiefCard} onClick={()=> navigate(member.path || "/")}>
+              <div>
+              <div className={styles.chiefAvatarRingWrapper} onClick={()=> navigate(member.path || "/")}>
+                  <img src={member.image} alt={member.name} />
+              </div>
+              <p className={styles.chiefRole}>{member.role}</p>
+              <p className={styles.chiefQuote}>{member.quote}</p>
+              <div className={styles.chiefKeep}>
+                  <h3 className={styles.chiefName}>{member.name}</h3>
+                  <p className={styles.chiefAge}>Age: {member.age}</p>
+                  <p className={styles.chiefServingSince}>Serving Since: {member.serving_since}</p>
+                  <div className={styles.chiefQualifications}>
+                          <h4>Qualifications:</h4>
+                          <div className={styles.qualificationList}>
+                              {member.qualifications.map((qualification, index) => (
+                                  <li key={index}>{qualification.qual} ({qualification.year})</li>
+                              ))}
+                          </div>
+                  </div>
+                  <div className={styles.chiefAchievements}>
+                          <h4>Achievements:</h4>
+                          <div className={styles.achievementList}>
+                              {member.achievements.map((achievement, index) => (
+                                  <li key={index}>{achievement.title} ({achievement.year})</li>
+                              ))}
+                          </div>
+                  </div>
+                  <p className={styles.chiefBio1}>Bio:</p>
+                  <p className={styles.chiefBio}>{member.bio}</p>
+              </div>
+                  
+              </div>
+              <div className={styles.chiefSocials}>
+                <span>🌐</span>
+                <span>✉️</span>
+              </div>
+            </div>
+          ))}
+        </div>
 
         {/* Strategic Visionaries */}
         <div className={styles.teamSec}>
